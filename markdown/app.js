e1f33880baf622a2f84b53bee0559003f6aa4ab9
idiotRun <doflamingod642@gmail.com>
2023-10-20 19:32:20 +0800

    submit markdown

---
+++ markdown/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+   const initialMarkdown = `# Welcome to Yusuf Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+return multiLineCode;
+}
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+- Some are bulleted.
+- With different indentation levels.
+- That look like this.
+
+
+1. And there are numbered lists too.
+2. Use just 1s if you want!
+3. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+
+   $('#editor').val(initialMarkdown);
+   $('#preview').html(marked(initialMarkdown, { breaks: true }));
+
+   // Update preview as you type in the editor
+   $('#editor').on('input', function () {
+      const markdown = $(this).val();
+      const html = marked(markdown, { breaks: true });
+      $('#preview').html(html);
+   });
+});
